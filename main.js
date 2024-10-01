811506f921e393acf37f9a6d04783e93a03bf9ce
martinasartori97 <martinasartori9@gmail.com>
2024-10-01 15:41:28 +0200

    edit js

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 console.log("hello world");
 
+// 1.preparazione
 //inizializzo una variabile che conterrà i numeri da 1 a 100
 const numerida1a100 = numerida1a100
-
-
-
 
 // inizializzo una variabile che stampi "fizz" per i multipli di 3
 const fizz = fizz
 
-
-
-
 // inizializzo una variabile che stampi "buzz" per i multipli di 5
 const buzz = buzz
 
-
-
-
 //inizializzo una variabile che stampi "fizzbuzz" sia per i multipli di 3 che 5
 const fizzbuzz = fizzbuzz
+
+
+// 2.raccolta dati
+// assegno alla variabile i numeri da 1 a 100
+// assegno alla variabile fizz i multipli di 3
+// assegno alla variabile buzz i multipli di 5
+// assegno alla variabile fizzbuzz i multipli di 3 e 5
+
+
+// 3.elaborazione
+// applico la stampa fizz ai multipli di 3
+// applico la stampa buzz ai multipli di 5
+// applico la stampa fizzbuzz ai multipli di 3 e 5
+
+
+// 4.output
+
