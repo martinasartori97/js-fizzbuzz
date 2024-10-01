fbed164a4b0f8965a88295bc4e66052e404f1f49
martinasartori97 <martinasartori9@gmail.com>
2024-10-01 15:15:42 +0200

    edit js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+
+//inizializzo una variabile che conterrà i numeri da 1 a 100
+const = numerida1a100
+
+
+// inizializzo una variabile che stampi "fizz" per i multipli di 3
+const = fizz
+
+
+
+
+// inizializzo una variabile che stampi "buzz" per i multipli di 5
+const = buzz
+
+
+
+
+//inizializzo una variabile che stampi "fizzbuzz" sia per i multipli di 3 che 5
+const = fizzbuzz
